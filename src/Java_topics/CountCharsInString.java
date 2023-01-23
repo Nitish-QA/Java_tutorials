9ad988b143254ab21987e6c9699acd07f1642559
Nitish Kaushal <nitish25j@gmail.com>
2023-01-23 11:49:21 +0530

    updated CountChars Prog

--- src/Java_topics/CountCharsInString.java
+++ src/Java_topics/CountCharsInString.java
@@ -5,35 +5,32 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class CountCharsInString {
-	
+
 	// Write a program to count occurrences of each characters in a string
-	
-	
-	
-	public static void getCharCount(String name) {
-		
+
+	public static void getCharCount(String st) {
+
 		Map<Character, Integer> charMap = new HashMap<Character, Integer>();
-		
-		char strArray[] = name.toLowerCase().toCharArray();
-		
-		for(char ch: strArray) {
-			if (charMap.containsKey(ch)){
-				charMap.put(ch, charMap.get(ch)+1);
-			}else {
+
+		char strArray[] = st.toLowerCase().toCharArray();
+
+		for (char ch : strArray) {
+			if (charMap.containsKey(ch)) {
+				charMap.put(ch, charMap.get(ch) + 1);
+			} else {
 				charMap.put(ch, 1);
 			}
 		}
-		System.out.println(name + " : " + charMap);
+		System.out.println(st + " : " + charMap);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a String: "+ '\n');
+		System.out.println("Enter a String: " + '\n');
 		String st = sc.nextLine();
 		getCharCount(st);
 
 	}
-
 }
