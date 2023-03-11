3d5dedf372177494af746c31e514a1e8895dabcc
Nitish Kaushal <nitish25j@gmail.com>
2023-03-11 12:37:34 +0530

    Revert "Merge branch 'testBranch'"
    This reverts commit e6321b68612cdae9ca22d0641efb5152dd51a335, reversing
    changes made to 0155e69c6685d576394c9b485bd6c5186d231e01.

--- src/Java_topics/Rough.java
+++ src/Java_topics/Rough.java
@@ -3,21 +3,37 @@ package Java_topics;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
-// WAP to count occurance of numbers in a string
+// WAP to display occurrence count of each characters in a string
 
 public class Rough {
 
 	public static void main(String[] args) throws IOException {
 		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter a name..");
+		String st = br.readLine();
 		
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Enter first number = ");
-			String st = sc.nextLine();
-			 System.out.println(" You have typed= " +st);
-	
+		//char ch[] = st.toCharArray();
 		
+
+		for (int i = 0; i <st.length(); i++) {
 			
+			char ch = st.charAt(i);
+			
+			int count=0;
+			for (int j = 0; j < st.length(); j++) {
+				if(ch==st.charAt(j)) {
+					//System.out.println(" current i value ="+i);
+					if(st.indexOf(st.charAt(i))>st.indexOf(st.charAt(j))) {	
+						//System.out.println("i="+st.indexOf(st.charAt(i))+" j= "+st.indexOf(st.charAt(j)));
+						//count--;
+						count++;
+					}
+					
+				}
+			}
+			System.out.println(ch+" count is "+count);
+		}
 	}
 }
