c573a2e8c4cf9b23ae3900cdcc09e570283d8c46
Nitish Kaushal <41166233+Nitish-QA@users.noreply.github.com>
2024-11-10 17:35:28 +0000

    updated UsefulCodes

--- src/Java_topics/UsefulCodes.java
+++ src/Java_topics/UsefulCodes.java
@@ -3,8 +3,12 @@ package Java_topics;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collector;
+import java.util.Map;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class UsefulCodes {
 
@@ -57,8 +61,8 @@ public class UsefulCodes {
         System.out.println(avg1);
 
         // Convert String to Integer (Parsing)
-        String str = "123";
-        int num1 = Integer.parseInt(str);
+        String str3 = "123";
+        int num1 = Integer.parseInt(str3);
         System.out.println(num1);
 
         // Check if a List Contains a Value
@@ -99,21 +103,73 @@ public class UsefulCodes {
         List<String> list13 = list12.stream().map(String :: toUpperCase).collect(Collectors.toList());
         System.out.println(list13);
 
+        // Join Elements of a List into a String
+        List<String> list14 = Arrays.asList("apple", "banana", "cherry");
 
+        String str4 = list14.stream().collect(Collectors.joining(", "));
+        System.out.println(str4);
 
+        // Find the Most Frequent Element in a List
+        List<String> list15 = Arrays.asList("a", "b", "a", "c", "b", "b");
+        Optional<String> mostFrequent = list15.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                                    .entrySet().stream().max(Map.Entry.comparingByValue()).map(Map.Entry::getKey);
+        System.out.println(mostFrequent.orElse("None"));
 
+        // Get the First N Elements of a List
+        List<Integer> list16 = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> list17 = list16.stream().limit(3).collect(Collectors.toList());
+        System.out.println(list17);
 
+        // Get the Last 2 Elements of a List
+        List<Integer> list18 = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> list19 = list18.stream().skip(list18.size() - 2).collect(Collectors.toList());
+        System.out.println(list19);
 
-    
+        // Check if a String is Numeric
+        String str5 = "12345";
+        Boolean b6 = str5.matches("\\d+");
+        System.out.println(b6);
 
+        //Find the Common Elements Between Two Lists
+        List<Integer> list20 = Arrays.asList(1, 2, 3, 4);
+        List<Integer> list21 = Arrays.asList(3, 4, 5, 6);
+        List<Integer> list22 = list20.stream().filter(list21 :: contains).collect(Collectors.toList());
+        System.out.println(list22);
 
+        // Find the Average of a List of Numbers
+        List<Integer> list23 = Arrays.asList(1, 2, 3, 4, 5);
+        OptionalDouble num3 = list23.stream().mapToInt(Integer :: intValue).average();
+        System.out.println(num3.orElse(0.0));
 
+        // Concatenate Multiple Strings
+        String str6 = Stream.of("Hi ", "my ", "name ", "is ", "nitish ").collect(Collectors.joining());
+        System.out.println(str6);
 
+        //Find the First Element of a Stream
+        List<Integer> list24 = Arrays.asList(10, 20, 30, 40, 50);
+        System.out.println(list24.stream().findFirst());
 
+        //Remove Leading and Trailing Whitespaces from a String
+        String str7 = "   Hello World!   ";
+        System.out.println(str7.trim());
 
+        //Convert a List of Strings to Lowercase
+        List<String> list25 = Arrays.asList("Apple", "Banana", "Cherry");
+        List<String> list26 = list25.stream().map(String :: toLowerCase).collect(Collectors.toList());
+        System.out.println(list26);
 
+        //Get the Length of Each String in a List
+        List<String> list27 = Arrays.asList("apple", "banana", "cherry");
+        List<Integer> list28 = list27.stream().map(String :: length).collect(Collectors.toList());
+        System.out.println(list28);
 
+        // Replace All Occurrences of a Character in a String
+        String str8 = "hello world";
+        System.out.println(str8.replace('o', '1'));
 
+        // Find the Index of the First Occurrence of a Character in a String
+        String str9 = "hello world";
+        System.out.println(str9.indexOf('r'));
 
     }
     
